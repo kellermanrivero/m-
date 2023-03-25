7bb49d8eafe2aa4f0ffb2c10401e7a956bbcb21c
Kellerman Rivero <riverokellerman@gmail.com>
2023-03-25 23:07:38 +0100

    Simplify sample1

--- samples/sample1/main.cpp
+++ samples/sample1/main.cpp
@@ -4,13 +4,12 @@
 int main() {
 
     SampleMetaModel x = {};
-    SampleMetaModel y = {};
 
-    for(auto [name, definition]: x.get_schema().get_properties()) {
-        std::cout << "Found definition " << definition.get_type_name() << " for property: " << name << std::endl;
+    for(auto [name, definition]: SampleMetaModel::get_schema().get_properties()) {
+        std::cout << "Found definition " << type_name(definition.get_type()) << " for property: " << name << std::endl;
     }
 
-    for(auto property: y.get_properties()) {
+    for(auto property: x.get_properties()) {
         auto definition = property->get_definition();
         std::cout << "Property '" << definition.get_name() << "' is of type: " << type_name(definition.get_type()) << std::endl;
     }
